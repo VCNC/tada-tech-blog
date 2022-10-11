8ef20248458e99e495af1b6138222f6a55df8099
joon <80660544+joon-vcnc@users.noreply.github.com>
2022-10-11 17:11:53 +0900

    og tag 수정 (#17)
    * 게시물 og:url 수정
    
    * twitter card meta tag 추가
    
    * 각 게시물에 og:description 누락된 부분 추가
    
    Co-authored-by: joon <rytt00@gmail.com>

--- src/components/Head/BlogList/index.tsx
+++ src/components/Head/BlogList/index.tsx
@@ -12,6 +12,12 @@ export const BlogListHead = () => {
       <meta property="og:description" content="기술로 이동의 한계를 돌파합니다" />
       <meta property="og:image" content="https://static.tadatada.com/resources/blog/img_thumbnail_link.png" />
       <meta property="og:url" content="https://blog-tech.tadatada.com/" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content="타다 TECH BLOG" />
+      <meta name="twitter:description" content="기술로 이동의 한계를 돌파합니다" />
+      <meta name="twitter:image" content="https://static.tadatada.com/resources/blog/img_thumbnail_link.png" />
+      <meta name="twitter:site" content="https://blog-tech.tadatada.com/" />
     </Helmet>
   )
 }
